62819a989a06d846131758193be13e999711c00c
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2023-06-25 13:06:50 +0300

    Up-to-date generation sample

--- runtime/LibSLGlobals.java
+++ runtime/LibSLGlobals.java
@@ -4,10 +4,10 @@ package runtime;
 import actual.java.util.OptionalDouble;
 import actual.java.util.OptionalDoubleAutomaton;
 
-class LibSLGlobals {
+public class LibSLGlobals {
+    public static final OptionalDouble EMPTY_OPTIONAL_DOUBLE;
+
     static {
         EMPTY_OPTIONAL_DOUBLE = new OptionalDouble(new OptionalDoubleAutomaton(OptionalDoubleAutomaton.__$lsl_States.Initialized, 0.0f, false));
     }
-
-    public final OptionalDouble EMPTY_OPTIONAL_DOUBLE;
 }
